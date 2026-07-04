ef40d85876d7e9d3ec3823770e6cf1054973cbe2
harshil6-lab <harshilkalsariya6@gmail.com>
2026-07-04 22:02:32 +0530

    chore : black formating applied manually

--- auditor/scanners/ebs_scanner.py
+++ auditor/scanners/ebs_scanner.py
@@ -3,7 +3,7 @@ from auditor.utils.retry import retry_on_throttle
 
 from auditor.aws.regions import get_regions
 
-from concurrent.futures import ThreadPoolExecutor,as_completed
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 def scan_unattached_volumes(region: str):
@@ -34,22 +34,21 @@ def scan_unattached_volumes(region: str):
 
 def scan_all_regions_ebs(profile_name=None):
     findings = []
-    
-        
+
     regions = get_regions(profile_name)
-     
-    with ThreadPoolExecutor(max_workers=min(8,len(regions))) as executor:
-         future_to_region = {}
-         for region in regions:
-             future = executor.submit(scan_unattached_volumes, region)
-             future_to_region[future] = region  
-        
-         for future in as_completed(future_to_region):
-             region = future_to_region[future]
-             try:
-                 unattached_volumes = future.result()
-                 findings.extend(unattached_volumes)
-             except Exception as e:
-                 print(f"[red]Error scanning region {region}: {e}[/red]")
+
+    with ThreadPoolExecutor(max_workers=min(8, len(regions))) as executor:
+        future_to_region = {}
+        for region in regions:
+            future = executor.submit(scan_unattached_volumes, region)
+            future_to_region[future] = region
+
+        for future in as_completed(future_to_region):
+            region = future_to_region[future]
+            try:
+                unattached_volumes = future.result()
+                findings.extend(unattached_volumes)
+            except Exception as e:
+                print(f"[red]Error scanning region {region}: {e}[/red]")
 
     return {"regions_scanned": len(regions), "findings": findings}
